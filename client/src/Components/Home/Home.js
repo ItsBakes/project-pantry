0e05259891702a9950ffb8456aec176f550e0a30
John <jobak1220@gmail.com>
2024-04-11 11:43:04 -0500

    add link. add classname to link

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -1,10 +1,12 @@
 import './Home.scss';
+import {Link} from 'react-router-dom'
+
 
 function Home () {
     <>
         <main className='Home'>
             <h1 className='Home__welcome'>welcome to project pantry!</h1>
-            <button className='Home__button'>let's get started</button>
+            <Link className='Home__link' to={}><button className='Home__button'>let's get started</button></Link>
         </main>
     </>
 }
