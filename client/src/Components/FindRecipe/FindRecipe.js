02f8033b77bd0c99e9b5e36806df96688f90d8b9
John <jobak1220@gmail.com>
2024-04-11 13:21:48 -0500

    add recipe filter component and recipe find page

---
+++ client/src/Components/FindRecipe/FindRecipe.js
@@ -0,0 +1,53 @@
+import './FindRecipe.scss'
+
+function FindRecipe() {
+    return (
+        <main className='FindRecipe'>
+            <form className='FindRecipe__form'>
+                <h3>search for recipes</h3>
+                <label className='FindRecipe__label'>search recipes:
+                    <input type='text' placeholder='search ingredients' className='FindRecipe__input'></input></label>
+                <h3>or pick what you have</h3>
+                <label>meal
+                    <select>
+                        <option>breakfast</option>
+                        <option>lunch</option>
+                        <option>dinner</option>
+                        <option>snack</option>
+                    </select></label>
+                <label>protein
+                    <select>
+                        <option>chicken</option>
+                        <option>beef</option>
+                        <option>pork</option>
+                        <option>fish</option>
+                    </select></label>
+                <label>style
+                    <select>
+                        <option>american</option>
+                        <option>asian</option>
+                        <option>french</option>
+                        <option>indian</option>
+                        <option>mediterranean</option>
+                        <option>mexican</option>
+                        <option>italian</option>
+                    </select></label>
+                <label>dietary restrictions
+                    <select>
+                        <option>dairy-free</option>
+                        <option>fish-free</option>
+                        <option>gluten-free</option>
+                        <option>peanut-free</option>
+                        <option>pescatarian</option>
+                        <option>vegan</option>
+                        <option>vegetarian</option>
+                    </select></label>
+
+
+
+            </form>
+        </main>
+    )
+}
+
+export default FindRecipe;
--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -3,12 +3,12 @@ import {Link} from 'react-router-dom'
 
 
 function Home () {
-    <>
+    return(
         <main className='Home'>
             <h1 className='Home__welcome'>welcome to project pantry!</h1>
             <Link className='Home__link' to={}><button className='Home__button'>let's get started</button></Link>
         </main>
-    </>
+    )
 }
 
 export default Home;
