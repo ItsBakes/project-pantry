e265d0a1465a28509f4a3d6ae91c7b237010ca5b
John <jobak1220@gmail.com>
2024-04-11 11:30:32 -0500

    add class names to home component

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -2,9 +2,11 @@ import './Home.scss';
 
 function Home () {
     <>
-        <main>
-            <h1>welcome to project pantry!</h1>
-            <button>let's get started</button>
+        <main className='Home'>
+            <h1 className='Home__welcome'>welcome to project pantry!</h1>
+            <button className='Home__button'>let's get started</button>
         </main>
     </>
 }
+
+export default Home;
